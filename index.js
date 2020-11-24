55943f5f0b9f9c119f25e87590111ab8aad2ebce
Maroš Jakubčo <maros.jakubco@plus4u.net>
2020-11-24 16:03:41 +0100

    Bot push

--- index.js
+++ index.js
@@ -9,13 +9,31 @@ const bot = new BootBot({
   appSecret: config.get('APP_SECRET')
 });
 
-bot.on('message', (payload, chat) => {
-	const text = payload.message.text;
-	console.log(`The user said: ${text}`);
+let greetings = ['Dobrý deň', 'Zdravím', 'Ahoj', 'Pozdravujem', 'Serus', 'Nazdar', 'dobry den', 'zdravim', 'dobry vecer', 'Dobrý večer']
+const buttons = [
+  { type: 'postback', title: 'Áno', payload: 'YES' },
+  { type: 'postback', title: 'Nie', payload: 'NO' }];
+
+
+const welcome = (zaplata) => {
+  zaplata.ask("Dobrý deň ! Vitajte na našej FB stránke. Prosím zadajte Váš email alebo telefónne číslo, aby sme Vás vedeli spätne kontaktovať : ", (payload, zaplata, data) => {
+    zaplata.sendTypingIndicator(1500).then(() => zaplata.ask("Ďakujeme ! S čím Vám môžeme pomôcť ?", (payload, zaplata, data) => {
+      zaplata.sendTypingIndicator(1500).then(() => zaplata.ask((zaplata) => {
+        zaplata.sendButtonTemplate("Ďakujeme ! Želáte si poslať nejaké fotky alebo screeny závady ?", buttons);
+      }, (payload, zaplata, data) => {
+        const answer = payload.postback.payload;
+        if(answer === "YES") zaplata.say("Pošlite nám teda fotky formou správy. Ďakujeme");
+        if(answer === "NO") zaplata.say("Ďakujeme, čoskoro Vás budeme kontaktovať.");
+      }))
+    }))
+  });
+}
+
+bot.hear(greetings, (payload, chat) => {
+  chat.conversation((zaplata) => {
+    zaplata.sendTypingIndicator(1500).then(() => welcome(zaplata))
+  })
 });
 
-bot.hear(['hello', 'hi'], (payload, chat) => {
-	console.log('The user said "hello" or "hi"!');
-});
 
 bot.start(port);
